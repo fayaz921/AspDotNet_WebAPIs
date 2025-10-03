b87f41059bf1e07fd1996ac5076a197ed2838cc3
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-03 22:49:40 +0500

    Commit
    Add User Entity & Add pakages

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Data/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿namespace AspDotNetCore_WebAPIs.Data.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
+
+        public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
+
+        public string Role { get; set; } = string.Empty;
+
+        public string? ImageUrl { get; set; }
+
+        public string Contact { get; set; } = string.Empty;
+
+        public string? OTP { get; set; }
+    }
+}

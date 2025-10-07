81cf194dbb09265b3a3122d0825141b0e36d8d9f
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-07 23:44:56 +0500

    Add Authentication Controller

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetCore_WebAPIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}

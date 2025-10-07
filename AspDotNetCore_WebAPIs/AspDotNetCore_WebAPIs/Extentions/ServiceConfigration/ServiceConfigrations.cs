e0786e24643c08f8b59effccffcf3e3669371303
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-08 00:13:16 +0500

    Add Service Configration

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Extentions/ServiceConfigration/ServiceConfigrations.cs
@@ -0,0 +1,14 @@
+﻿using AspDotNetCore_WebAPIs.Services.Implementation;
+using AspDotNetCore_WebAPIs.Services.Interfaces;
+
+namespace AspDotNetCore_WebAPIs.Extentions.ServiceConfigration
+{
+    public static class ServiceConfigrations
+    {
+        public static IServiceCollection AddServiceConfigration(this IServiceCollection services)
+            => services.AddScoped<IUserService, UserService>()
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen();
+
+    }
+}

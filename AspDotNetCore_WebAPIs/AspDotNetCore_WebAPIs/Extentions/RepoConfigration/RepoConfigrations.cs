f301772d43a50d107ddb41d695f751f8a8558675
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-08 00:13:04 +0500

    Add repoConfigration

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Extentions/RepoConfigration/RepoConfigrations.cs
@@ -0,0 +1,15 @@
+﻿using AspDotNetCore_WebAPIs.Data;
+using AspDotNetCore_WebAPIs.Repositories.Implementation;
+using AspDotNetCore_WebAPIs.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspDotNetCore_WebAPIs.Extentions.RepoConfigration
+{
+    public static class RepoConfigrations
+    {
+        public static IServiceCollection AddRepoConfigration(this IServiceCollection services, IConfiguration configuration)
+            => services.AddScoped<IUserRepo, UserRepo>()
+            .AddDbContext<AppDBContext>(options => options.UseSqlServer(configuration.GetConnectionString("MyConn")));
+
+    }
+}

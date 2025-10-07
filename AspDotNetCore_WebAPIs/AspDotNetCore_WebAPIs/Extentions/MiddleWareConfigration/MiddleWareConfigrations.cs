7d71e407dcb4726518e0d0e2c840a56bc3e2cad3
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-08 00:13:29 +0500

    Add Middleware Configration

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Extentions/MiddleWareConfigration/MiddleWareConfigrations.cs
@@ -0,0 +1,21 @@
+﻿namespace AspDotNetCore_WebAPIs.Extentions.MiddleWareConfigration
+{
+    public static class MiddleWareConfigrations
+    {
+        public static WebApplication AddMiddleWareConfigrations(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+            return app;
+        }
+    }
+}

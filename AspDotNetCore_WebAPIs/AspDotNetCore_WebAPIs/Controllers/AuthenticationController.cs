5dd8304d9ebf1c7a8690a20522194ccc6fa3ba40
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-07 23:44:43 +0500

    Add UserController

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Controllers/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using AspDotNetCore_WebAPIs.Dtos.Authentication;
+using AspDotNetCore_WebAPIs.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetCore_WebAPIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IUserService userService;
+        public AuthenticationController(IUserService _userService)
+        {
+            userService = _userService;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
+        {
+            var user = await userService.RegisterAsync(userRegisterDto);
+            if (user == null)
+            {
+                return BadRequest("User Email Already Exist.");
+            }
+            return Ok(user);
+        }
+    }
+}

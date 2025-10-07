891a2d8110ba769090ff3e68d2dce3cfca126d33
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-08 00:13:50 +0500

    Clean program.cs and call extension classes

--- AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Program.cs
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Program.cs
@@ -1,4 +1,7 @@
 using AspDotNetCore_WebAPIs.Data;
+using AspDotNetCore_WebAPIs.Extentions.MiddleWareConfigration;
+using AspDotNetCore_WebAPIs.Extentions.RepoConfigration;
+using AspDotNetCore_WebAPIs.Extentions.ServiceConfigration;
 using AspDotNetCore_WebAPIs.Repositories.Implementation;
 using AspDotNetCore_WebAPIs.Repositories.Interfaces;
 using AspDotNetCore_WebAPIs.Services.Implementation;
@@ -6,30 +9,15 @@ using AspDotNetCore_WebAPIs.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+//Add Db and Repo
+builder.Services.AddRepoConfigration(builder.Configuration);
 // Add services to the container.
-
+builder.Services.AddServiceConfigration();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConn")));
-builder.Services.AddScoped<IUserRepo, UserRepo>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.AddMiddleWareConfigrations();   
 app.Run();

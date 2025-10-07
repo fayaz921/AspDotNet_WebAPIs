436f7cd2bdd5a2c4f965da759948750f500ef945
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-07 22:24:04 +0500

    Add User repo

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Repositories/Implementation/UserRepo.cs
@@ -0,0 +1,27 @@
+﻿using AspDotNetCore_WebAPIs.Data;
+using AspDotNetCore_WebAPIs.Data.Entities;
+using AspDotNetCore_WebAPIs.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspDotNetCore_WebAPIs.Repositories.Implementation
+{
+    public class UserRepo : IUserRepo
+    {
+        private readonly AppDBContext appDBContext;
+        public UserRepo(AppDBContext _appdb)
+        {
+            appDBContext = _appdb;
+        }
+        public async Task<User> RegisterAsync(User user)
+        {
+            var UserCheck = await appDBContext.Users.Where(u => u.Email == user.Email).FirstOrDefaultAsync();
+            if(UserCheck == null)
+            {
+                await appDBContext.Users.AddAsync(user);
+                await appDBContext.SaveChangesAsync();
+                return user;
+            }
+            return null!;
+        }
+    }
+}

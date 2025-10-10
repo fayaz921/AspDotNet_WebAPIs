30a33d8639442d957f5a7ea827469025bedea94f
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-10 15:59:06 +0500

    add api response class and methods

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/APIResponse/APIResponses.cs
@@ -0,0 +1,28 @@
+﻿namespace AspDotNetCore_WebAPIs.APIResponse
+{
+    public class APIResponses<T>
+    {
+        public T? Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public T? Error { get; set; }
+
+        public static APIResponses<T> SuccessResponse(T? data)
+        {
+            return new APIResponses<T>
+            {
+                Data = data,
+                IsSuccess = true,
+            };
+        }
+
+        public static APIResponses<T> FailureResponse(T? error)
+        {
+            return new APIResponses<T>
+            {
+                IsSuccess = false,
+                Error = error
+            };
+        }
+       
+    }
+}

e21a530b7fd7c3a15adddce0c1e6727585a186f3
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-10 23:24:17 +0500

    add authservice interface

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using AspDotNetCore_WebAPIs.APIResponse;
+using AspDotNetCore_WebAPIs.Data.Entities;
+using AspDotNetCore_WebAPIs.Dtos.Authentication;
+using AspDotNetCore_WebAPIs.Dtos.Users;
+
+namespace AspDotNetCore_WebAPIs.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<APIResponses<GetUserDto>> RegisterAsync(UserRegisterDto userRegister);
+      
+    }
+}

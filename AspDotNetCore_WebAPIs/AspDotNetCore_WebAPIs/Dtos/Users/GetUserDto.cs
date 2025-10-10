285da6c057bb4101a38951ebcf0b43b376a59780
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-10 16:00:21 +0500

    add username in getuserdto

--- AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Dtos/Users/GetUserDto.cs
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Dtos/Users/GetUserDto.cs
@@ -1,5 +1,5 @@
 ﻿namespace AspDotNetCore_WebAPIs.Dtos.Users
 {
-    public record GetUserDto(Guid UserId,string FirstName, string LastName, string Email, string Role, string ImageUrl, string Contact);
+    public record GetUserDto(Guid UserId,string FirstName, string LastName, string Email, string Role, string ImageUrl, string Contact,string UserName);
     
 }

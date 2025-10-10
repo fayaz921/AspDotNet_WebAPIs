3447a6b5dc2ab510092773df963b7067f21cc811
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-10 16:02:12 +0500

    add iauthentication interface

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Repositories/Interfaces/IAuthenticationRepo.cs
@@ -0,0 +1,10 @@
+﻿using AspDotNetCore_WebAPIs.Data.Entities;
+
+namespace AspDotNetCore_WebAPIs.Repositories.Interfaces
+{
+    public interface IAuthenticationRepo
+    {
+        Task<User> RegisterAsync(User user);
+        Task<User> LoginAsync(User user);
+    }
+}

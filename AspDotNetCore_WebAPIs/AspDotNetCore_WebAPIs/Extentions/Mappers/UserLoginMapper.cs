b8971ccbf75b8f691e5fb124eadbc4e505cdacb0
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-11 23:27:29 +0500

    add mapper of logindto

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Extentions/Mappers/UserLoginMapper.cs
@@ -0,0 +1,19 @@
+﻿using AspDotNetCore_WebAPIs.Data.Entities;
+using AspDotNetCore_WebAPIs.Dtos.Authentication;
+
+namespace AspDotNetCore_WebAPIs.Extentions.Mappers
+{
+    public static class UserLoginMapper
+    {
+        public static User Map(this UserLoginDto userLoginDto)
+        {
+
+            return new User
+            {
+                UserName = userLoginDto.UserIdentifier,
+                Email = userLoginDto.UserIdentifier,
+                Contact = userLoginDto.UserIdentifier,
+            };
+        }
+    }
+}

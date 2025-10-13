4bba0b67c2c7a2814c1b1b091f61f0af87f84d23
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-13 15:01:20 +0500

    add service for jwt

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Utilities/JWTTokenService.cs
@@ -0,0 +1,40 @@
+﻿using AspDotNetCore_WebAPIs.Data.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AspDotNetCore_WebAPIs.Utilities
+{
+    public class JWTTokenService : IJWTTokenService
+    {
+        private readonly IConfiguration _configuration;
+        public JWTTokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        public async Task<string> CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role,user.Role),
+            };
+
+            var tokenkey =  _configuration.GetSection("Token").Value;
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenkey!)); //! null-forgiving operator
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds,
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}

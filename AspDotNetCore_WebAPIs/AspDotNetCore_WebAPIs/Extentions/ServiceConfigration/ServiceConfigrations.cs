7e0c6002179311089fb864ca7dd635f2fc354716
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-14 23:08:06 +0500

    add jwt authentication

--- AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Extentions/ServiceConfigration/ServiceConfigrations.cs
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Extentions/ServiceConfigration/ServiceConfigrations.cs
@@ -1,6 +1,8 @@
 ﻿using AspDotNetCore_WebAPIs.Services.Implementation;
 using AspDotNetCore_WebAPIs.Services.Interfaces;
 using AspDotNetCore_WebAPIs.Utilities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 
@@ -8,15 +10,17 @@ namespace AspDotNetCore_WebAPIs.Extentions.ServiceConfigration
 {
     public static class ServiceConfigrations
     {
+        //Service Configuration
         public static IServiceCollection AddServiceConfigration(this IServiceCollection services)
                                                 => services.AddScoped<IUserService, UserService>()
                                                            .AddEndpointsApiExplorer()
                                                            .AddScoped<IAuthenticationService, AuthenticationService>()
-                                                           .AddScoped<IJWTTokenService,JWTTokenService>();
-                                                           
+                                                           .AddScoped<IJWTTokenService, JWTTokenService>();
 
+
+        //Swagger Configuration
         public static IServiceCollection AddSwaggerCongigration(this IServiceCollection services)
-        
+
           => services.AddSwaggerGen(options =>
             {
                 options.SwaggerDoc("v1", new OpenApiInfo()
@@ -26,15 +30,37 @@ namespace AspDotNetCore_WebAPIs.Extentions.ServiceConfigration
                 });
                 options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
                 {
-                   In = ParameterLocation.Header,
-                   Name = "Authorization",
-                   Scheme = "Bearer",
-                   Type = SecuritySchemeType.ApiKey,
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    Type = SecuritySchemeType.ApiKey,
                 });
                 options.OperationFilter<SecurityRequirementsOperationFilter>();
             });
-        
-                                               
+
+        //JWT Authentication
+        public static IServiceCollection AddAuthentions(this IServiceCollection services, IConfiguration configuration)
+        {
+            var tokenkey = configuration.GetSection("Token").Value;
+            var issuer = configuration.GetSection("IssuerKey").Value;
+            var audience = configuration.GetSection("AudienceKey").Value;
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidAudience = audience,
+                            ValidIssuer = issuer,
+                            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(tokenkey!)),
+                            ValidateIssuerSigningKey = true,
+                            ValidateLifetime = true,
+                        };
+                    });
+            return services;
+        }
+
+
+
 
     }
 }

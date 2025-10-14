f644b5c03589358071900c23b7cc2f05c7cc3bad
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-14 23:08:32 +0500

    inject jwtauthentication

--- AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Program.cs
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Program.cs
@@ -1,19 +1,14 @@
-using AspDotNetCore_WebAPIs.Data;
 using AspDotNetCore_WebAPIs.Extentions.MiddleWareConfigration;
 using AspDotNetCore_WebAPIs.Extentions.RepoConfigration;
 using AspDotNetCore_WebAPIs.Extentions.ServiceConfigration;
-using AspDotNetCore_WebAPIs.Repositories.Implementation;
-using AspDotNetCore_WebAPIs.Repositories.Interfaces;
-using AspDotNetCore_WebAPIs.Services.Implementation;
-using AspDotNetCore_WebAPIs.Services.Interfaces;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 //Add Db and Repo
 builder.Services.AddRepoConfigration(builder.Configuration);
 // Add services to the container.
 builder.Services.AddServiceConfigration()
-                .AddSwaggerCongigration();
+                .AddSwaggerCongigration()
+                .AddAuthentions(builder.Configuration);
 builder.Services.AddControllers();
 
 

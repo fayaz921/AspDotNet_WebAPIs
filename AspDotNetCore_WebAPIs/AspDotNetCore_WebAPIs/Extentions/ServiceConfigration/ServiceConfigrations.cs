ef4ac296a90b2be8ec03024ffb7c0b26fe4e8a35
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-14 11:24:10 +0500

    Add extension method for swaggercongigration

--- AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Extentions/ServiceConfigration/ServiceConfigrations.cs
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Extentions/ServiceConfigration/ServiceConfigrations.cs
@@ -1,15 +1,40 @@
 ﻿using AspDotNetCore_WebAPIs.Services.Implementation;
 using AspDotNetCore_WebAPIs.Services.Interfaces;
+using AspDotNetCore_WebAPIs.Utilities;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace AspDotNetCore_WebAPIs.Extentions.ServiceConfigration
 {
     public static class ServiceConfigrations
     {
         public static IServiceCollection AddServiceConfigration(this IServiceCollection services)
-            => services.AddScoped<IUserService, UserService>()
-            .AddEndpointsApiExplorer()
-            .AddSwaggerGen()
-            .AddScoped<IAuthenticationService, AuthenticationService>();
+                                                => services.AddScoped<IUserService, UserService>()
+                                                           .AddEndpointsApiExplorer()
+                                                           .AddScoped<IAuthenticationService, AuthenticationService>()
+                                                           .AddScoped<IJWTTokenService,JWTTokenService>();
+                                                           
+
+        public static IServiceCollection AddSwaggerCongigration(this IServiceCollection services)
+        
+          => services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "AspDotNetCore_WebAPIs",
+                    Version = "v1",
+                });
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                   In = ParameterLocation.Header,
+                   Name = "Authorization",
+                   Scheme = "Bearer",
+                   Type = SecuritySchemeType.ApiKey,
+                });
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+        
+                                               
 
     }
 }

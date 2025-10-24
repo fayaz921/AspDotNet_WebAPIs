0f9ba3db270f01e2b9ae05ee1985c5a8ccfedd4c
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-24 21:55:25 +0500

    add interface of password encryptor

---
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Shared/IPasswordEncryptor.cs
@@ -0,0 +1,8 @@
+﻿namespace AspDotNetCore_WebAPIs.Shared
+{
+    public interface IPasswordEncryptor
+    {
+        void CreatePasswordHashandSalt(string password, out byte[] hash, out byte[] salt);
+        bool VerifyPasswordhashandsalt(string password, byte[] Storedhash, byte[] Storedsalt);
+    }
+}

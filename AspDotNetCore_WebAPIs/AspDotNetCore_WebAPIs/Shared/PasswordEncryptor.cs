c78c8b2e378bd7fee0564c04b4772e55b27cc68c
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-10-24 21:56:11 +0500

    add implentation for passwordencryptor

--- AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Shared/PasswordEncryptor.cs
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Shared/PasswordEncryptor.cs
@@ -3,20 +3,23 @@ using System.Text;
 
 namespace AspDotNetCore_WebAPIs.Shared
 {
-    public static class PasswordEncryptor
+    public  class PasswordEncryptor : IPasswordEncryptor
     {
-        public static void CreatePasswordHashandSalt(string password, out byte[] hash, out byte[] salt)
+        public  void CreatePasswordHashandSalt(string password, out byte[] hash, out byte[] salt)
         {
             using var hmac = new HMACSHA512();
             salt = hmac.Key;
             hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
         }
 
-        public static bool VerifyPasswordhashandsalt(string password, byte[] Storedhash, byte[] Storedsalt)
+        public  bool VerifyPasswordhashandsalt(string password, byte[] Storedhash, byte[] Storedsalt)
         {
             using var hmac = new HMACSHA512(Storedsalt);
             var computehash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
             return computehash.SequenceEqual(Storedhash);
         }
+
+
+     
     }
 }

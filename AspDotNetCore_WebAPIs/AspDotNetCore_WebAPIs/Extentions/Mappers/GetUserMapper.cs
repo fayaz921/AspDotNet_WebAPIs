84d41168514ed0af144bdf153f4bc63853577ecc
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-23 09:17:16 +0500

    Update GetUserMapper.cs

--- AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Extentions/Mappers/GetUserMapper.cs
+++ AspDotNetCore_WebAPIs/AspDotNetCore_WebAPIs/Extentions/Mappers/GetUserMapper.cs
@@ -9,5 +9,21 @@ namespace AspDotNetCore_WebAPIs.Extentions.Mappers
         {
             return new GetUserDto(user.UserId,user.FirstName,user.LastName,user.Email,user.Contact,user.Role.ToString(),user.ImageUrl!,user.UserName);
         }
+
+        public static List<GetUserDto> Map(this List<User> users)
+        {
+            return users.Select(users=> new GetUserDto
+            (
+                users.UserId,
+                users.FirstName,
+                users.LastName,
+                users.Email,
+                users.Contact,
+                users.Role.ToString(),
+                users.ImageUrl!,
+                users.UserName
+                ))
+                .ToList();
+        }
     }
 }
